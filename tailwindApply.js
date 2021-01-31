cccbb2459f311348706b8f60a04213860b5a1412
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-01-31 09:05:20 +0200

    Update tailwindApply.js

--- tailwindApply.js
+++ tailwindApply.js
@@ -2,7 +2,8 @@ function tailwindApply(){
     var x = document.querySelectorAll('*');
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].className += getComputedStyle(x[i]).getPropertyValue('--apply');
+        x[i].className += ' ' + getComputedStyle(x[i]).getPropertyValue('--apply');
+        x[i].className = x[i].className.replace(/  /g, " ");
     }
 }
 window.onload = function() {
